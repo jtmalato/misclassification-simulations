363da4bbfddb9dc7e1b798c921c8fe236ce56364
JoaoMalato <joaomalato.93@gmail.com>
2021-11-29 15:42:46 +0100

    Serological study
    Hypothetical simulation on serological study. Contruction of scenario 2 results figure and table.

--- scripts/simulation-on-hypothetical-data.R
+++ scripts/simulation-on-hypothetical-data.R
@@ -6,135 +6,11 @@
 # Libraries ---------------------------------------------------------------
 library(data.table)
 library(here)
-# library(magrittr)
-# library(ggplot2)
 
 
-# Functions ---------------------------------------------------------------
+# Load functions ----------------------------------------------------------
 
-# true cases of theta1* through input of theta0 OR
-p1_t <- function(p0, or) {
-  out <- (p0 * or) / (1 - p0 + (p0 * or))
-  return(out)
-}
-# p1_t(0.25, 1.5)
-
-# p1_t <- function(p0, or) {
-#   out <- (p0 * or) / (1 + (p0 * (or - 1)))
-#   return(out)
-# }
-
-# estimate suspected cases p1
-# this parameter is dependent of 5 variables:
-#   1. misclassification rate
-#   2. serological test sensitiviy
-#   3. serological test specificity
-#   4. healthy contorls exposure probability
-#   5. true CFS exposure probability
-theta1 <- function(gamma, se, sp, theta0, theta1_true) {
-  apparent_exp_pos    <- se * gamma * theta0
-  apparent_nonexp_pos <- (1-sp) * gamma * (1-theta0)
-  true_exp_pos        <-  se * (1-gamma) * theta1_true
-  tru_nonexp_pos      <- (1-sp) * (1-gamma) * (1-theta1_true)
-  return(apparent_exp_pos + apparent_nonexp_pos + true_exp_pos + tru_nonexp_pos)
-}
-
-# order and estimate p-values
-get_p_value <- function(data) {
-  return(suppressWarnings(chisq.test(matrix(data, ncol = 2), correct = FALSE)$p.value))
-}
-
-# confidence interval based on vector
-confidence_interval <- function(vector, interval = 0.95) {
-  # standard deviation of sample
-  v_sd <- sd(vector)
-  # sample size
-  v_n <- length(vector)
-  # mean of sample
-  v_mean <- mean(vector)
-  # error according to t distribution
-  v_error <- qt((interval + 1)/2, df = v_n - 1) * v_sd / sqrt(v_n)
-  # confidence interval as a vector
-  return(c(v_mean - v_error, v_mean + v_error))
-}
-
-
-# Simulation structure ----------------------------------------------------
-
-serology_simulations <- function(sim = 10000,
-                                 n_control = 100,
-                                 n_cfs = 100,
-                                 p0 = 0.5,
-                                 or_t = 2,
-                                 se = 1,
-                                 sp = 1,
-                                 gamma = 0.5) {
-  require(data.table)
-  p1_true <- p1_t(p0, or_t)
-  # Controls ------------------------------------------
-  # true exposed/non-exposed healthy controls
-  xp_c <- data.table::data.table(xp = rbinom(sim, n_control, p0))[, nxp := n_control-xp][]
-  # structure of serological test
-  outcome_c <- xp_c[, .(t_pos = rbinom(sim, xp, se),
-                        t_neg = rbinom(sim, nxp, sp))][
-                          , `:=` (f_neg = xp_c$xp-t_pos,
-                                  f_pos = xp_c$nxp-t_neg)][]
-  # serological test outcome
-  serotest_c <- outcome_c[, .(pos = t_pos + f_pos,
-                              neg = t_neg + f_neg)]
-
-  # CFS------------------------------------------------
-  # apparent/false
-  n_apparent <- rbinom(sim, n_cfs, gamma)
-  xp_f <- data.table::data.table(xp = rbinom(sim, n_apparent, p0))[, nxp := n_apparent-xp][]
-  # structure of serological test
-  outcome_f <- xp_f[, .(t_pos = rbinom(sim, xp, se),
-                        t_neg = rbinom(sim, nxp, sp))][
-                          , `:=` (f_neg = xp_f$xp-t_pos,
-                                  f_pos = xp_f$nxp-t_neg)][]
-  # serological test outcome
-  serotest_f <- outcome_f[, .(pos = t_pos + f_pos,
-                              neg = t_neg + f_neg)]
-  # true
-  n_true <- n_cfs - n_apparent
-
-  xp_t <- data.table::data.table(xp = rbinom(sim, n_true, p1_true))[, nxp := n_true-xp][]
-  # structure of serological test
-  outcome_t <- xp_t[, .(t_pos = rbinom(sim, xp, se),
-                        t_neg = rbinom(sim, nxp, sp))][
-                          , `:=` (f_neg = xp_t$xp-t_pos,
-                                  f_pos = xp_t$nxp-t_neg)][]
-  # serological test outcome
-  serotest_t <- outcome_t[, .(pos = t_pos + f_pos,
-                              neg = t_neg + f_neg)]
-  # gather data created
-  data_obs <- cbind(hc=serotest_c, cfs=serotest_f+serotest_t)
-  vector_p_value <- apply(data_obs, 1, get_p_value)
-  p_value <- vector_p_value < 0.05
-  if(sum(is.na(p_value)) != 0) {p_value[is.na(p_value)] <- FALSE}
-  ci <- confidence_interval(p_value)
-  # overall exposure probability on any suspected case
-  p1 <- theta1(gamma=gamma, se=se, sp=sp, theta0=p0, theta1_true=p1_true)
-  # estimated OR of cases agains hc based on risk factor exposure
-  odds <- (p1 * (1-p0)) / ((1-p1) * p0)
-
-  out <- c(
-    sim         = sim,
-    sample_size = n_control,
-    sensitivity = se,
-    specificity = sp,
-    misrate     = gamma,
-    p           = mean(p_value),
-    p_min       = ci[1],
-    p_max       = ci[2],
-    overall_or  = odds,
-    or_t        = or_t,
-    theta0      = p0,
-    theta1_true = p1_true,
-    theta1      = p1
-  )
-  return(out)
-}
+source(here("scripts/simulation-functions.R"))
 
 
 # Run simulations ---------------------------------------------------------
@@ -169,6 +45,8 @@ sim_gene <-
                                             gamma = gamma)]
   }
 sim_gene_dt <- as.data.table(sim_gene)
+
+# fwrite(simulation_gene, here("data", paste0(paste(Sys.Date(), "simulation-structure-candidate-gene", sep = "_"), ".csv")))
 # fwrite(sim_gene_dt, here("data", paste0(paste(Sys.Date(), "simulation-results-candidate-gene", sep = "_"), ".csv")))
 
 
@@ -196,6 +74,8 @@ sim_sero <-
                                                 gamma = gamma)]
   }
 sim_sero_dt <- as.data.table(sim_sero)
+
+# fwrite(simulation_sero, here("data", paste0(paste(Sys.Date(), "simulation-structure-serology", sep = "_"), ".csv")))
 # fwrite(sim_sero_dt, here("data", paste0(paste(Sys.Date(), "simulation-results-serology", sep = "_"), ".csv")))
 
 # end
--- scripts/simulation-results-and-figures.R
+++ scripts/simulation-results-and-figures.R
@@ -18,6 +18,11 @@ dt1 <- fread(here("data/2021-11-26_simulation-results-candidate-gene.csv"))
 dt1[, ss := factor(sample_size)]
 dt1[, or_f := factor(or_t, levels = rev(as.character(unique(or_t))))]
 
+dt2 <- fread(here("data/2021-11-29_simulation-results-serology.csv"))
+dt2[, ss := factor(sample_size)]
+dt2[, `:=` (se_f = factor(sensitivity, levels = rev(as.character(unique(sensitivity)))),
+            sp_f = factor(specificity, levels = rev(as.character(unique(specificity)))))]
+
 
 # Theme -------------------------------------------------------------------
 theme_set(
@@ -129,12 +134,67 @@ rbind(dcast(dt1_p80[sample_size == 100],  or_f ~ theta0, value.var = "misrate"),
       dcast(dt1_p80[sample_size == 2500], or_f ~ theta0, value.var = "misrate"),
       dcast(dt1_p80[sample_size == 5000], or_f ~ theta0, value.var = "misrate"))
 
+
 # Serology test -----------------------------------------------------------
 
 
 # Figure ------------------------------------------------------------------
 
+gg2 <- dt2 %>%
+  ggplot(aes(misrate, p, group = ss)) +
+  annotate("rect", xmin=0, xmax=1, ymin=0.80, ymax=1, alpha=0.2, fill="gray35") +
+  facet_grid(sp_f ~ se_f, labeller = label_bquote(rows = pi[sp] == .(as.character(sp_f)), cols = pi[se] == .(as.character(se_f)))) +
+  geom_line(aes(colour = ss), size = 1.1) +
+  geom_hline(yintercept = 0.05, alpha = 0.8) +
+  scale_colour_manual(values = darken(viridis(6), 0.1)) +
+  scale_y_continuous(limits = c(0,1),
+                     breaks = sort(c(0.05, seq(0,1, 0.2))),
+                     labels = c("", expression(alpha), "0.2","0.4","0.6","0.8", "1")) +
+  scale_x_continuous(limits = c(0, 1), breaks = seq(0,1,0.2), labels = c("0","0.2","0.4","0.6","0.8", "1")) +
+  coord_cartesian(expand = FALSE, clip = "off") +
+  labs(x = expression(paste("Misclassification rate,")~gamma),
+       y = "Probability of rejecting null hypothesis",
+       colour = expression(paste("Sample size")~(n[0]==n[1]))) +
+  guides(colour = guide_legend(nrow = 1, override.aes = list(size=2, alpha = 0.9)))
+# png
+ggsave(here("figures/simulations-serology.png"), gg2,
+       scale = 1, dpi = 320, width = 14, height = 14)
+# pdf LaTeX
+ggsave(here("figures/simulations-serology.pdf"), gg2,
+       scale = 1, dpi = 320, width = 14, height = 14, device = cairo_pdf)
+
+
+
 # Table -------------------------------------------------------------------
+
+# variables to filter by
+# all possible combinations
+dt2_c <- CJ(or = dt2[, unique(se_f)], theta = dt2[, unique(theta0)], n = dt1[, unique(sample_size)])
+
+# get individual datasets and select maximum misclassification when p>=80%
+power_p <- function(data, odds, theta, n, p_above) {
+  d <- data[or_t == as.numeric(odds) & theta0 == as.numeric(theta) & sample_size == as.numeric(n)]
+  values <- ifelse(c(d[p >= p_above, .N] != 0, d[p >= p_above, .N] != 0),
+                   c(d[d[p >= p_above, .N], misrate], tail(d[p >= p_above, p], 1)),
+                   c(0, max(d[, p])))
+  d[1, .(or_t, theta0, sample_size,
+         misrate = values[1], p_80 = values[2])]
+}
+
+# save it
+# setorder(table_p_80, n, theta0, -or_t, misrate)
+fwrite(dt1_p80, here("data/candidate-gene-power-above-80.csv"))
+
+dt1_p80[, or_f := factor(or_t, levels = c(10, 5, 2, 1.5, 1.25))]
+dt1_p80[, thetaf := factor(theta0, levels = c(0.5, 0.25, 0.10, 0.05))]
+
+rbind(dcast(dt1_p80[sample_size == 100],  or_f ~ theta0, value.var = "misrate"),
+      dcast(dt1_p80[sample_size == 250],  or_f ~ theta0, value.var = "misrate"),
+      dcast(dt1_p80[sample_size == 250],  or_f ~ theta0, value.var = "misrate"),
+      dcast(dt1_p80[sample_size == 1000], or_f ~ theta0, value.var = "misrate"),
+      dcast(dt1_p80[sample_size == 2500], or_f ~ theta0, value.var = "misrate"),
+      dcast(dt1_p80[sample_size == 5000], or_f ~ theta0, value.var = "misrate"))
+
 
 
 # end
